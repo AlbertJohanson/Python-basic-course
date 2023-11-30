ac99fc2ac49c2b509f04db0f53c0b6f11ae77fb8
Albert Johanson <albert@studio31.io>
2023-11-30 00:11:24 -0600

    Add Python code files

---
+++ platzi-course-python/01_vars.py
@@ -0,0 +1,15 @@
+print('Hola, este es el archivo 01')
+
+#Estas son variables
+my_fist_name = 'Albert'
+my_second_name = 'Johanson'
+my_age = 23
+
+# input 
+
+my_name  = input("What is your name?: ")
+
+
+
+#aqui contatenamos cada variable y formamos un texto
+print(my_fist_name + ' ' + my_second_name + ' y tengo ' + str(my_age) + ' años')
---
+++ platzi-course-python/02_dataTypes.py
@@ -0,0 +1,17 @@
+#string
+my_name = 'Albert'
+print('My name is ', my_name)
+
+# int
+my_age = 23
+print('My name is ', my_name, ' and I am ', my_age, ' old')
+
+#boolean
+is_single = True
+
+print('The value of is_single is ',is_single)
+
+
+#inputs 
+my_age = input('Can you give me your age: ')
+print('Oh so you are ', my_age, ' years old')
