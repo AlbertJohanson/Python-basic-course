a7f806dc2a60ce6a6de1f72d8f260b2e0daf69ed
Albert Johanson <albert@studio31.io>
2024-11-16 22:25:00 -0600

    DELETE ALL AND STAR A NEW COURSE

--- fstrings/fstrings.py
+++
@@ -1,7 +0,0 @@
-nombre = "Albert"
-edad = 22
-
-# print(f"Hola {nombre}. Tienes {edad} años")
-# print(f"Hola {nombre}. El año que viene tendrás {edad + 1} años")
-
-print(f"'Haz lo que quieras siempre y cuando ames lo que hagas' -{nombre}")
